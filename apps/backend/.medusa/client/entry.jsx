12be033637a4841fdc29e8db89f85629f45c0807
Hotor Benjamin <hotorben001@gmail.com>
2026-06-15 14:54:22 +0000

    7-15/6

--- apps/backend/.medusa/client/entry.jsx
+++ apps/backend/.medusa/client/entry.jsx
@@ -2,7 +2,7 @@ import App from "@medusajs/dashboard";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+ 
 import plugin0 from "@medusajs/draft-order/admin"
 
 let root = null
