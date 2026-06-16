c9081f2486514187f1f29039771e02c6a57eaf42
Hotor Benjamin <hotorben001@gmail.com>
2026-06-16 19:07:09 +0000

    2-16/6

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -10,10 +10,12 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 module.exports = defineConfig({
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
   projectConfig: {
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
